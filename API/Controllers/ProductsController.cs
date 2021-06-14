1b28f4a070e040b27b8c31293fdb3138c619bb98
M7esn10 <M7esn10@gmail.com>
2021-06-14 15:30:38 +0300

    SEEDING DATA, NEW METHODS FOR GETTING TYPES AND BRANDS -END OF SECTION3-

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -17,16 +17,27 @@ namespace API.Controllers {
             _repo = repo;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts() {
-            var products = await _repo.GetProductsAsync();
-            return Ok(products);
-        }
-
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id) {
             return await _repo.GetProductByIdAsync(id);
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts() {
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands() {
+            var productsBrands = await _repo.GetProductBrandsAsync();
+            return Ok(productsBrands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes() {
+            var productTypes = await _repo.GetProductTypesAsync();
+            return Ok(productTypes);
+        }
     }
 }
